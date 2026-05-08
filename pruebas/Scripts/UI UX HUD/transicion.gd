673eda4fa5e80f8fa8708c64ad735c00285ad8a3
Alex Nescuik <alexiamomos@gmail.com>
2026-05-08 02:19:30 -0600

    Commit 42 - Inicio, Transiciones y Menú totales
    yupii

--- pruebas/Scripts/UI UX HUD/transicion.gd
+++ pruebas/Scripts/UI UX HUD/transicion.gd
@@ -3,9 +3,14 @@ extends CanvasLayer
 @onready var anim = %AnimationPlayer
 
 func cambiar_escena(ruta_escena: String):
+	var escena_actual = get_tree().current_scene
+	
+	for hijo in escena_actual.get_children():
+		if hijo is CanvasLayer or hijo is Control:
+			var tween = create_tween()
+			tween.tween_property(hijo, "modulate:a", 0.0, 0.4) 
+	
 	anim.play("fade_to_black")
 	await anim.animation_finished
-	
 	get_tree().change_scene_to_file(ruta_escena)
-	
 	anim.play("fade_from_black")
